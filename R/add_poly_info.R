04e47c4187941e6477581f7c3b4c4337b18dcb54
jakelawlor <jakealawlor@gmail.com>
2025-09-03 06:39:15 -0400

    add sp_poly_info

--- R/add_poly_info.R
+++ R/add_poly_info.R
@@ -16,13 +16,22 @@ add_poly_info <- function(data, type = "SA"){
   poly_info <- switch(
     type,
     "SA" = readRDS(system.file("extdata", "poly_info.rds", package = "BioShiftR")),
-    "SP" = readRDS(system.file("extdata", "poly_info.rds", package = "BioShiftR"))
+    "SP" = readRDS(system.file("extdata", "sp_poly_info.rds", package = "BioShiftR"))
   )
 
   merged <- switch(
     type,
-    "SA" = data |> dplyr::left_join(poly_info, by = dplyr::join_by(article_id, poly_id))
+    "SA" = data |> dplyr::left_join(poly_info, by = dplyr::join_by(article_id, poly_id)),
+    "SP" = data |> dplyr::left_join(poly_info, by = dplyr::join_by(article_id, poly_id, sp_name_checked))
+
   )
+
+  if(type == "SP"){
+    n_missing <- sum(is.na(merged$lat_cent_deg))
+    if(n_missing > 0){
+      warning(call. = F, paste0("Not all shifts have associated species-specific polygon values. ",n_missing," NAs returned."))
+    }
+  }
 
   return(merged)
 
