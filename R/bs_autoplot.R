73f8f3d44d3b019a332184174bad07af060a6b04
jakelawlor <jakealawlor@gmail.com>
2026-01-02 00:27:59 -0500

    edit cv vignette

--- R/bs_autoplot.R
+++ R/bs_autoplot.R
@@ -1,9 +1,9 @@
 #' Title
 #'
-#' @param species
+#' @param species Name of a select species for which to plot all documented shifts.
 #' @param article
 #' @param type
-#' @param plottype
+#' @param plottype The type of plot to return. Options are
 #'
 #' @returns
 ## ' @export
@@ -12,7 +12,8 @@
 bs_autoplot <- function(species = c("Troglodytes troglodytes","Fringilla coelebs"),
                         article = NULL,
                         type = c("LAT","ELE"),
-                        plottype = "point"){
+                        plottype = "point",
+                        facet = NULL){
 
 
   sp <- stringr::str_replace(species, " ","_")
@@ -27,7 +28,7 @@ bs_autoplot <- function(species = c("Troglodytes troglodytes","Fringilla coelebs
   if(any(sp_find$same == F)){
     n <- sum(sp_find$same == F)
     warning(paste0("Species names have been resolved. This plot contains ", n,
-                   " shifts for which author-provided species names have changed."))
+                   " shifts for which author-provided species name was different."))
   }
 
   if(nrow(sp_find) == 0){
@@ -35,38 +36,101 @@ bs_autoplot <- function(species = c("Troglodytes troglodytes","Fringilla coelebs
   }
 
 
-  avg <- sp_find %>%
-    group_by(type, param) %>%
-    summarize(calc_rate = mean(calc_rate))
-  sp_find %>%
-    ggplot2::ggplot(aes(x = article_id,
-                        y = calc_rate,
-                        color = param)) +
-    ggplot2::geom_point() +
-    facet_grid(param~type,
-               scales = "free_x") +
-    geom_hline(yintercept = 0) +
-    geom_hline(data = avg,
-               aes(yintercept = calc_rate,
-                   color = param),
-               linetype = "dashed",
-             linewidth = .5,
-             #outside = T,
-             show.legend = F) +
-    theme_bw()+
-    theme(panel.grid = element_blank())
+  if(plottype == "map"){
 
-  sp_find %>%
-    ggplot(aes(x = param,
-               y = calc_rate,
-               color = param)) +
-    geom_hline(yintercept = 0) +
-    geom_point(position = position_jitter(width = .1, height = 0),
-               alpha = .8) +
-    geom_boxplot(width = .2,
-                 fill = "transparent",
-                 color = "black",
-                 outliers = F)
+    filename <- #switch(type,
+                      # "SA" = "sa_polygons_simplified.rds",
+                      # "SP" =
+      "sp_polygons_simplified.rds"#)
+
+    # check if polygon gpkg has already been downloaded
+    all_proj_files <-
+      list.files(recursive = T,
+                 include.dirs = F,
+                 full.names = F)
+
+
+    # check if filename already exists
+    exists <- any(stringr::str_detect(all_proj_files, filename))
+
+    if(exists == F){
+      stop("Polygons not found locally. Please use download_polygons(), or specify directory if they are downloaded outside of default.")
+    }
+
+
+    path <- all_proj_files[which(stringr::str_detect(all_proj_files,filename))]
+
+    polys <- readRDS(path) %>%
+      filter(sp_name_checked %in% sp) %>%
+      right_join(sp_find, by = dplyr::join_by(article_id, poly_id, sp_name_checked))
+
+    bbox <- sf::st_bbox(sf::st_buffer(sf::st_union(polys),2))
+
+    return <- ggplot2::ggplot(data = polys) +
+      ggplot2::geom_sf(data = rnaturalearth::ne_countries(returnclass = "sf") |>
+                         sf::st_crop(bbox)) +
+      ggplot2::geom_sf( ggplot2::aes(fill = calc_rate,
+                                     color = calc_rate),
+                        alpha = .8) +
+     # ggplot2::scale_fill_viridis_c() +
+      ggplot2::scale_fill_gradient2(high = "tomato2", low = "cyan4", mid = "#dfa9f5",
+                                    trans = ggallin::ssqrt_trans) +
+      ggplot2::scale_color_gradient2(high = "tomato2", low = "cyan4", mid = "#dfa9f5",
+                                     trans = ggallin::ssqrt_trans) +
+      ggplot2::theme_bw() +
+      #ggplot2::facet_wrap(~param)+
+      ggplot2::coord_sf(expand=F)
+
+    if(facet){
+      return <- return +
+        ggplot2::facet_wrap(~get(facet))
+
+    }
+
+
+  }
+
+
+
+
+  #avg <- sp_find %>%
+  #  group_by(type, param) %>%
+  #  summarize(calc_rate = mean(calc_rate))
+ # sp_find %>%
+ #   ggplot2::ggplot(aes(x = article_id,
+ #                       y = calc_rate,
+ #                       color = param)) +
+ #   ggplot2::geom_point() +
+ #   facet_grid(param~type,
+ #              scales = "free_x") +
+ #   geom_hline(yintercept = 0) +
+ #   geom_hline(data = avg,
+ #              aes(yintercept = calc_rate,
+ #                  color = param),
+ #              linetype = "dashed",
+ #            linewidth = .5,
+ #            #outside = T,
+ #            show.legend = F) +
+ #   theme_bw()+
+ #   theme(panel.grid = element_blank())
+#
+
+  if(plottype == "boxplot"){
+
+   return <-  sp_find %>%
+      ggplot(aes(x = param,
+                 y = calc_rate,
+                 color = param)) +
+      geom_hline(yintercept = 0) +
+      geom_point(position = position_jitter(width = .1, height = 0),
+                 alpha = .8) +
+      geom_boxplot(width = .2,
+                   fill = "transparent",
+                   color = "black",
+                   outliers = F)
+  }
+
+  return(return)
 
 }
 
--- R/get_shifts.R
+++ R/get_shifts.R
@@ -2,10 +2,10 @@
 #'
 #' Get species' range shift values from the BioShifts database, filtered by taxon, study type or geography. BioShifts includes range shift observations of over 31,000 taxa within studies conducted around the world, published between
 #'
-#' @param group Rough taxonomic subgroups for which to pull bioshifts data. Options are Algae, Birds, Fish, Fungi, Mammals, Marine Invertebrates, Nonvascular Plants, Reptiles and Amphibians, Terrestrial Invertebrates, and Vascular Plants, or All (default). This shortcut is meant to provide a coarse subsetting for data exploration, but for more precise taxonomic filtering, see add_taxo().
-#' @param realm Subset of study realms for which to uplaod range shift data. Options are "Mar" (marine), "Ter" (terrestrial), or "All" (default).
-#' @param continent Continent of studies for which to upload BioShifts data. Options include "North America", "South America", "Africa", "Europe", "Asia", "Oceania", "High Seas", or "All".
-#' @param type Gradient over which to extract range shifts. Options are "ELE" for elevational shifts, or "LAT" for latitudinal shifts.
+#' @param group Rough taxonomic subgroups for which to pull bioshifts data. Options are `Algae`, `Birds`, `Fish`, `Fungi`, `Mammals`, `Marine Invertebrates`, `Nonvascular Plants`, `Reptiles and Amphibians`, `Terrestrial Invertebrates`, and `Vascular Plants`, or `All` (default). This shortcut is meant to provide a coarse subsetting for data exploration, but for more precise taxonomic filtering, see `add_taxo()`.
+#' @param realm Subset of study realms for which to uplaod range shift data. Options are `Mar` (marine), `Ter` (terrestrial), or `All` (default).
+#' @param continent Continent of studies for which to upload BioShifts data. Options include `North America`, `South America`, `Africa`, `Europe`, `Asia`, `Oceania`, `High Seas`, or `All`.
+#' @param type Gradient over which to extract range shifts. Options are `ELE` for elevational shifts, or `LAT` for latitudinal shifts.
 #'
 #' @returns Minimal data frame of calculated and author-reported species range shift values.
 #' @export
