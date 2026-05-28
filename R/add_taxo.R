ebb268da4a275fc2bd5d07246fa9bad14445fb8f
bwwolfe <barrett.wolfe@utas.edu.au>
2026-05-29 08:27:17 +1000

    fix param name (data in use, df in the roxygen documnetation)

--- R/add_taxo.R
+++ R/add_taxo.R
@@ -2,7 +2,7 @@
 #'
 #' add_taxo() merges taxonomic classification to range shifts uploaded with the get_shifts() function.
 #'
-#' @param df input shifts dataset from get_shifts() function.
+#' @param data input shifts dataset from get_shifts() function.
 #'
 #' @returns Range shifts dataset with added columns containing taxonomic classification information for every species name, as written in the original publication.
 #' @export
